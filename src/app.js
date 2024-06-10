e41752015b72ba86b519b8d2a1c7071a8e40645d
Danomolina <danomolina@gmail.com>
2024-06-10 19:54:52 +0000

    generador_nombre

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extensions = ['.com'];
+
+  let domains = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+          for (let k = 0; k < noun.length; k++) {
+              for (let l = 0; l < extensions.length; l++) {
+                  domains.push(`${pronoun[i]}${adj[j]}${noun[k]}${extensions[l]}`);
+              }
+          }
+      }
+  }
+
+  for (let n = 0; n < domains.length; n++) {
+      console.log(domains[n]);
+  }
 };
